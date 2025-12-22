42f63fd48ef3b9d36c68b65a52b3e3e2ca575212
interactsolutions <38389204+interactsolutions@users.noreply.github.com>
2025-12-22 14:00:57 +0500

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+// dashboard.js - Alias file for data_processor.js
+// This file redirects to the main JavaScript file
+
+console.log('AgriVista Dashboard loading...');
+
+// Simple redirect to the main JavaScript file
+document.write('<script src="data_processor.js"></script>');
